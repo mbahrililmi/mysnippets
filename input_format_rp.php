9a582c53f0b81350a2a6f7decadfee2771ed4da0
bahrilio <mbahrililmi2@gmail.com>
2022-04-09 13:58:04 +0700

    make inputan format otomatis

---
+++ input_format_rp.php
@@ -0,0 +1,25 @@
+<!-- memerlukan id inputan -->
+<!-- id inputan sekarang yaitu .price -->
+
+<script>
+    var price = document.getElementById('price');
+    price.addEventListener('keyup', function(e) {
+        price.value = formatRupiah(this.value, 'Rp. ');
+    });
+
+    function formatRupiah(angka, prefix) {
+        var number_string = angka.replace(/[^,\d]/g, '').toString(),
+            split = number_string.split(','),
+            sisa = split[0].length % 3,
+            rupiah = split[0].substr(0, sisa),
+            ribuan = split[0].substr(sisa).match(/\d{3}/gi);
+
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+        }
+
+        rupiah = split[1] != undefined ? rupiah + ',' + split[1] : rupiah;
+        return prefix == undefined ? rupiah : (rupiah ? 'Rp. ' + rupiah : '');
+    }
+</script>
