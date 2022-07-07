69c52d87a1e0c36b6ecd540a31d377a1aa9c1767
mbahrililmi <mbahrililmi2.com>
2022-07-07 11:49:50 +0800

    penambahan alert from sweet alert

---
+++ make_alert_from_sweetalert2.php
@@ -0,0 +1,42 @@
+@push('after-script')
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        @if(session() - > has('success'))
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'success',
+            title: '{{ session("success") }}'
+        })
+        @endif
+
+        @if(session() - > has('warning'))
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'warning',
+            title: '{{ session("warning") }}'
+        })
+        @endif
+    </script>
+@endpush
